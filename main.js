e6ad2525393c56de5fbf9142153b1e54b691f11f
xbabhahkckjclkjclcjjclacjjclcjclc <lol@gmail.com>
2022-05-09 17:55:49 +0400

    Add GPL-3.0 stuff

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
-console.log("Hello, World!");
+/*
+This file is part of Hello.
+
+Hello is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+Hello is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+console.log('Alex Joseph., hereby disclaims all copyright interest in the program “Hello” written by Alex Joseph.')
+
+
+console.log("signature of Alex Joseph 5/9/2022")
+console.log("Alex Joseph, Programmer")
+console.log("Hello, World!")
 let spam = "hello";
 let eggs = spam;
 console.log(eggs);
