9e6576d272d20210101100580fde610c1710ca67
xbabhahkckjclkjclcjjclacjjclcjclc <lol@gmail.com>
2022-05-11 15:59:00 +0400

    fix CJS

---
+++ lol.js
@@ -0,0 +1,13 @@
+/*
+This file is part of Hello.
+
+Hello is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by t\
+he Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+Hello is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABIL\
+ITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with Hello. If not, see <https://www.gnu.org/licenses/>.
+*/
+let lol = { lol: "lol" }
+module.exports = lol
